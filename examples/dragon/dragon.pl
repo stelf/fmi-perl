1f5137ce5ecbc84ff9be3ef7550eaa82c28c943f
Peter Pentchev <roam@ringlet.net>
2015-03-06 18:19:59 +0200

    Turtle graphics and the -m mode cmdline option.

--- examples/dragon/dragon.pl
+++ examples/dragon/dragon.pl
@@ -4,11 +4,57 @@ use 5.012;
 use strict;
 use warnings;
 
+use Getopt::Std;
+
+sub usage($) {
+	my ($err) = @_;
+	my $s = <<USAGE
+Usage: dragon [-m mode] generation
+
+Available options:
+	-m	the output mode; use '-m list' for a list
+
+The generation must be a positive integer.
+USAGE
+	;
+
+	if ($err) {
+		die($s);
+	} else {
+		print $s;
+	}
+}
+
+sub version()
+{
+	say 'dragon 0.01';
+}
+
 MAIN:
 {
-	if (@ARGV != 1 || $ARGV[0] !~ /^[1-9][0-9]*$/) {
-		die("Usage: dragon generation\n\nThe generation must be a positive integer.\n");
+	my %opts;
+	getopts('hm:V', \%opts) or usage(1);
+	version() if $opts{V};
+	usage(0) if $opts{h};
+	exit(0) if $opts{h} || $opts{V};
+
+	my $mode = 'turns';
+	if (exists($opts{m})) {
+		my %modes = (
+			'turns'		=> 'the sequence of turns',
+			'turtle'	=> 'turtle graphics commands',
+		);
+
+		if ($opts{m} eq 'list') {
+			say 'Available output modes:';
+			say "$_\t$modes{$_}" for sort keys %modes;
+			exit(0);
+		}
+		usage(1) unless exists $modes{$opts{m}};
+		$mode = $opts{m};
 	}
+
+	usage(1) unless @ARGV == 1 && $ARGV[0] =~ /^[1-9][0-9]*$/;
 	my $generation = $ARGV[0];
 
 	my @turns = (1);
@@ -16,5 +62,13 @@ MAIN:
 		@turns = (@turns, 1, map { 1 - $_ } reverse @turns);
 	}
 
-	say @turns;
+	if ($mode eq 'turns') {
+		say @turns;
+	} elsif ($mode eq 'turtle') {
+		say 'forward 10';
+		for my $turn (@turns) {
+			say $turn? 'right': 'left', ' 90';
+			say 'forward 10';
+		}
+	}
 }
--- examples/dragon/t/01-valid.t
+++ examples/dragon/t/01-valid.t
@@ -4,8 +4,13 @@ use 5.012;
 use strict;
 use warnings;
 
-use Test::More tests => 2;
+use Test::More tests => 4;
 use Test::Command;
 
 stdout_is_eq('perl dragon.pl 1', "1\n");
 stdout_is_eq('perl dragon.pl 4', "110110011100100\n");
+
+# The -m mode command-line option
+stdout_is_eq('perl dragon.pl -m turns 4', "110110011100100\n");
+stdout_is_eq('perl dragon.pl -m turtle 2',
+    "forward 10\nright 90\nforward 10\nright 90\nforward 10\nleft 90\nforward 10\n");
